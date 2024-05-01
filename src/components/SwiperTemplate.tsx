58770194d9c9e69bef02dbb4fb9e7880f4a32001
Pharadol <pharadol.n@gmail.com>
2024-05-01 22:50:03 +0700

    build: change for build test

---
+++ src/components/SwiperTemplate.tsx
@@ -0,0 +1,50 @@
+// "use client";
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay, Pagination, Navigation } from "swiper/modules";
+// import { SwiperOptions } from "swiper/types";
+
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+// import { banners } from "@/assets/data/banners";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// type SwiperOptionType = SwiperOptions & {
+//   pagination?: { type: string; clickable: boolean };
+//   modules?: any[];
+//   autoplay?: { delay: number; disableOnInteraction: boolean };
+// };
+
+// function SwiperTemplate({ items }) {
+//   const itemss = [{ title: "s" }];
+//   const swiperSettings: SwiperOptionType = {
+//     navigation: true,
+//     pagination: { type: "bullets", clickable: true },
+//     modules: [Autoplay, Pagination, Navigation],
+//     autoplay: {
+//       delay: 4000,
+//       disableOnInteraction: false,
+//     },
+//     loop: true,
+//   };
+//   return (
+//     <Swiper {...swiperSettings} className="h-auto w-full">
+//       {itemss.map((item, index) => (
+//         <SwiperSlide key={index}>
+//           <article>
+//             <div>
+//               <div>
+//                 {item.title}
+//               </div>
+//             </div>
+//           </article>
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// }
+
+// export default SwiperTemplate;
